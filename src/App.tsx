14cf12973e63480e0b3b85c12f76798c0b517041
Nyfaria DragonMoose <kinaria3324@gmail.com>
2024-05-23 19:25:35 -0500

    fixed weird hover issue.

--- src/App.tsx
+++ src/App.tsx
@@ -23,20 +23,32 @@ function App() {
                 <img className={"sidebar-logo"} src={logo} alt="logo"/>
                 <div className={"sidebar-title"}>Modding with Nyfaria</div>
                 <div className={"line-1"}></div>
-                <div className={"sidebar-option"} id={"sidebar-tutorials"}><FaHome/>Home</div>
-                <div className={"sidebar-option"} id={"sidebar-tutorials"} onClick={onTutClick}>
-                    <IoSchoolSharp/>Tutorials {!tutorialsCollapsed ? <BiSolidDownArrow className={"float-right"}/> :
-                    <BiSolidUpArrow className={"float-right"}/>}
+                <div className={"sidebar-option"}><FaHome/>Home</div>
+                <div className={tutorialsCollapsed ? "sidebar-tutorials-close" : "sidebar-tutorials-open"}>
+                    <div className={"sidebar-option"} onClick={onTutClick}>
+                        <IoSchoolSharp/>Tutorials {!tutorialsCollapsed ? <BiSolidDownArrow className={"float-right"}/> :
+                        <BiSolidUpArrow className={"float-right"}/>}
+                    </div>
+                    <div className={!tutorialsCollapsed ? "sidebar-sub-option sidebar-option-hover" : "collapsed"}
+                         id={"sidebar-tutorials"}>1.12.2 Modding
+                    </div>
+                    <div className={!tutorialsCollapsed ? "sidebar-sub-option" : "collapsed"}
+                         id={"sidebar-tutorials"}>1.20.1 Modding
+                    </div>
                 </div>
-                <div className={!tutorialsCollapsed ? "sidebar-sub-option sidebar-option-hover" : "collapsed"}
-                     id={"sidebar-tutorials"}>1.12.2 Modding
-                </div>
-                <div className={!tutorialsCollapsed ? "sidebar-sub-option" : "collapsed"}
-                     id={"sidebar-tutorials"}>Coming Soon...
-                </div>
-                <div className={"sidebar-option"} id={"sidebar-projects"}  onClick={onProjClick}>
-                    <TbBrandMinecraft/>Projects {!projectsCollapsed ? <BiSolidDownArrow className={"float-right"}/> :
-                    <BiSolidUpArrow className={"float-right"}/>}
+
+                <div className={projectsCollapsed ? "sidebar-projects-close" :"sidebar-projects-open"}>
+                    <div className={"sidebar-option"} onClick={onProjClick}>
+                        <TbBrandMinecraft/>Projects {!projectsCollapsed ?
+                        <BiSolidDownArrow className={"float-right"}/> :
+                        <BiSolidUpArrow className={"float-right"}/>}
+                    </div>
+                    <div className={!projectsCollapsed ? "sidebar-sub-option sidebar-option-hover" : "collapsed"}
+                         id={"sidebar-tutorials"}>Solo/Collab Projects
+                    </div>
+                    <div className={!projectsCollapsed ? "sidebar-sub-option" : "collapsed"}
+                         id={"sidebar-tutorials"}> Team Projects
+                    </div>
                 </div>
             </div>
             <div onClick={onClick}
